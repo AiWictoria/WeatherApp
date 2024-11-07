257a53250619d595e4e47ea7c4847e44c5030b42
Linda <lindachch11@gmail.com>
2024-11-07 13:41:43 +0100

    Added new code to html and css

---
+++ Grupparbete/assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+
+function switchToLightTheme() {
+    console.log("Switching to Normal Theme"); 
+    document.body.setAttribute("data-theme", "normal");
+}
+
+// Function to switch to Swedish theme
+function switchToSwedishTheme() {
+    console.log("Switching to Swedish Theme");
+    document.body.setAttribute("data-theme", "swedish");
+}
