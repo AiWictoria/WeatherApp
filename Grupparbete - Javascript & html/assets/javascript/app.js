5f03e139f386bdb585f27c3eaed9050f3c4aefb6
Johan <Johan.0hrbom@gmail.com>
2024-11-07 12:36:19 +0100

    Lagt till de resterande dagarna I forecasten (min och max temp och vilka dagar det är)

--- Grupparbete - Javascript & html/assets/javascript/app.js
+++ Grupparbete - Javascript & html/assets/javascript/app.js
@@ -1,66 +1,3 @@
-// const apiUrlManila = "https://api.openweathermap.org/data/2.5/weather?q=Manila&units=metric&appid=81dcd8cceea19c746d25742109494663";
-// const apiUrlStockholm = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&appid=81dcd8cceea19c746d25742109494663";
-// const apiUrlKiev = "https://api.openweathermap.org/data/2.5/weather?q=Kiev&units=metric&appid=81dcd8cceea19c746d25742109494663";
-// const apiUrlDamaskus = "https://api.openweathermap.org/data/2.5/weather?q=Damaskus&units=metric&appid=81dcd8cceea19c746d25742109494663";
-
-// async function checkWeatherManila(){
-//     const response = await fetch(apiUrlManila);
-//     var data = await response.json();
-
-//     console.log(data);
-
-// document.querySelector(".cityManila").innerHTML = data.name;
-// document.querySelector(".tempManila").innerHTML = Math.round(data.main.temp) + "°C";
-// document.querySelector(".humidityManila").innerHTML = "Humidity: " + data.main.humidity + "%";
-// document.querySelector(".windManila").innerHTML = "Wind speed: " + data.wind.speed + " km/h";
-
-// }
-
-// async function checkWeatherStockholm(){
-//     const response = await fetch(apiUrlStockholm);
-//     var data = await response.json();
-
-//     console.log(data);
-
-// document.querySelector(".cityStockholm").innerHTML = data.name;
-// document.querySelector(".tempStockholm").innerHTML = Math.round(data.main.temp) + "°C";
-// document.querySelector(".humidityStockholm").innerHTML = "Humidity: " + data.main.humidity + "%";
-// document.querySelector(".windStockholm").innerHTML = "Wind speed: " + data.wind.speed + " km/h";
-
-// }
-
-// async function checkWeatherKiev(){
-//     const response = await fetch(apiUrlKiev);
-//     var data = await response.json();
-
-//     console.log(data);
-
-// document.querySelector(".cityKiev").innerHTML = data.name;
-// document.querySelector(".tempKiev").innerHTML = Math.round(data.main.temp) + "°C";
-// document.querySelector(".humidityKiev").innerHTML = "Humidity: " + data.main.humidity + "%";
-// document.querySelector(".windKiev").innerHTML = "Wind speed: " + data.wind.speed + " km/h";
-
-// }
-
-// async function checkWeatherDamaskus(){
-//     const response = await fetch(apiUrlDamaskus);
-//     var data = await response.json();
-
-//     console.log(data);
-
-// document.querySelector(".cityDamaskus").innerHTML = data.name;
-// document.querySelector(".tempDamaskus").innerHTML = Math.round(data.main.temp) + "°C";
-// document.querySelector(".humidityDamaskus").innerHTML = "Humidity: " + data.main.humidity + "%";
-// document.querySelector(".windDamaskus").innerHTML = "Wind speed: " + data.wind.speed + " km/h";
-
-// }
-
-
-// checkWeatherManila();
-// checkWeatherStockholm();
-// checkWeatherKiev();
-// checkWeatherDamaskus();
-
 const apiUrlManila = "http://api.weatherapi.com/v1/forecast.json?key=70923cd2adb646fc90190851240711&q=Manila&days=5&aqi=no&alerts=no";
 
 async function checkWeatherManila(){
@@ -77,12 +14,29 @@ async function checkWeatherManila(){
         document.querySelector(".windManila").innerHTML = "Wind speed: " + data.current.wind_kph + " km/h";
         document.querySelector(".mintempforecast1Manila").innerHTML = Math.round(data.forecast.forecastday[1].day.mintemp_c) + "°C";
         document.querySelector(".maxtempforecast1Manila").innerHTML = Math.round(data.forecast.forecastday[1].day.maxtemp_c) + "°C";
+        document.querySelector(".mintempforecast2Manila").innerHTML = Math.round(data.forecast.forecastday[2].day.mintemp_c) + "°C";
+        document.querySelector(".maxtempforecast2Manila").innerHTML = Math.round(data.forecast.forecastday[2].day.maxtemp_c) + "°C";
+        document.querySelector(".mintempforecast3Manila").innerHTML = Math.round(data.forecast.forecastday[3].day.mintemp_c) + "°C";
+        document.querySelector(".maxtempforecast3Manila").innerHTML = Math.round(data.forecast.forecastday[3].day.maxtemp_c) + "°C";
+        document.querySelector(".mintempforecast4Manila").innerHTML = Math.round(data.forecast.forecastday[4].day.mintemp_c) + "°C";
+        document.querySelector(".maxtempforecast4Manila").innerHTML = Math.round(data.forecast.forecastday[4].day.maxtemp_c) + "°C";
         
         const weekday = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"];
     
-        const d = new Date(data.forecast.forecastday[1].date);
-        let day = weekday[d.getDay()];
-        document.querySelector(".dayforecast1Manila").innerHTML = day;
+        const d1 = new Date(data.forecast.forecastday[1].date);
+        const d2 = new Date(data.forecast.forecastday[2].date);
+        const d3 = new Date(data.forecast.forecastday[3].date);
+        const d4 = new Date(data.forecast.forecastday[4].date);
+
+        let day1 = weekday[d1.getDay()];
+        let day2 = weekday[d2.getDay()];
+        let day3 = weekday[d3.getDay()];
+        let day4 = weekday[d4.getDay()];
+
+        document.querySelector(".dayforecast1Manila").innerHTML = day1;
+        document.querySelector(".dayforecast2Manila").innerHTML = day2;
+        document.querySelector(".dayforecast3Manila").innerHTML = day3;
+        document.querySelector(".dayforecast4Manila").innerHTML = day4;
     }
     
 checkWeatherManila();
