71fd881e63dfe55ac3684fc35e2504ec7ba3675d
AiWictoria <Viktoria.pakhrova@outlook.com>
2024-11-14 21:28:16 +0100

    moved select element to footer near compare button and added script to hide and show it in compare and regular mode. Added second color for text for better user experience

--- Grupparbete - Javascript & html/assets/compare.js
+++ Grupparbete - Javascript & html/assets/compare.js
@@ -9,11 +9,13 @@ function landChange() {
         document.getElementsByClassName('land-2')[0].className = 'land-2 compare land-box compare';
         document.getElementsByClassName('forcast-land-1')[0].className = 'forcast-land-1 compare forcast-box compare';
         document.getElementsByClassName('forcast-land-2')[0].className = 'forcast-land-2 compare forcast-box compare';
+        document.getElementsByClassName('selectboxpos')[0].className = 'selectboxpos compare';
     }
     else {
         document.getElementsByClassName('land-1')[0].className = 'land-1 land-box';
         document.getElementsByClassName('land-2')[0].className = 'land-2 land-box';
         document.getElementsByClassName('forcast-land-1')[0].className = 'forcast-land-1 forcast-box';
         document.getElementsByClassName('forcast-land-2')[0].className = 'forcast-land-2 forcast-box';
+        document.getElementsByClassName('selectboxpos')[0].className = 'selectboxpos';
      }
 }
